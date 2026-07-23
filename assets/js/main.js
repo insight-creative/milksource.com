530d7abe69acf17541e875d0125f0031111eadfb
Amanda Giannunzio <amanda@insightcreative.com>
2026-07-23 18:32:37 +0000

    Updated 1 file via CloudCannon.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,9 @@ const scrollTop = () => window.scrollY;
 
 const scrollDetect = (collapse, expand) => {
   const currentScroll = scrollTop();
+  if (scrollState === -1) {
+    scrollState = 0;
+  }
   if (currentScroll > scrollState) {
     collapse();
   } else {
